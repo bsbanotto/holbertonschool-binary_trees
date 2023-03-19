7caa0728f7e558eeebdca730f812bd1dfdae411d
bsbanotto <ben.sbanotto@gmail.com>
2023-03-18 20:42:43 -0500

    not sure i handled not having a sibling

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - Finds the sibling of a given node
+ * @node: node to find sibling of
+ *
+ * Return: pointer to sibling node or NULL
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left != node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
