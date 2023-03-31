af391b1ec8e0f2cead6ffb0b78c94c2acba9edc4
bsbanotto <ben.sbanotto@gmail.com>
2023-03-31 13:03:52 -0500

    Commit for task 0

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,6 +1,28 @@
 #include "binary_trees.h"
 
 /**
+ * isBSTUtil - Utility function to test if BST is valid
+ * @tree: pointer to tree node to test
+ * @min: some variable
+ * @max: some variable
+ *
+ * Return: 1 if true, 0 if false
+ */
+
+int isBSTUtil(const binary_tree_t *tree, int min, int max)
+{
+	if (tree == NULL)
+		return (1);
+
+	if (tree->n < min || tree->n > max)
+		return (0);
+
+	return (isBSTUtil(tree->left, min, tree->n - 1) &&
+	isBSTUtil(tree->right, tree->n + 1, max));
+}
+
+
+/**
  * binary_tree_is_bst - Checks if a binary tree is is a valid BST
  * @tree: pointer to root node of tree to check
  *
@@ -9,7 +31,5 @@
 
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	return (0);
+	return (isBSTUtil(tree, INT_MIN, INT_MAX));
 }
--- binary_trees.h
+++ binary_trees.h
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 
 /* Structure */
@@ -60,6 +61,7 @@ int is_perfect_recursive(const binary_tree_t *tree, int depth, int level);
 /* Prototypes for 0x08. Binary Trees 1 */
 
 int binary_tree_is_bst(const binary_tree_t *tree);
+int isBSTUtil(const binary_tree_t *tree, int min, int max);
 bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
