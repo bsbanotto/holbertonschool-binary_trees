82c1f969ad58eeea83b400e6c4763430ba1e1229
bsbanotto <ben.sbanotto@gmail.com>
2023-03-31 13:05:22 -0500

    Fixed check for NULL node

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -31,5 +31,7 @@ int isBSTUtil(const binary_tree_t *tree, int min, int max)
 
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 	return (isBSTUtil(tree, INT_MIN, INT_MAX));
 }
